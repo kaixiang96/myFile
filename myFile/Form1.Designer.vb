a6084bb80faf1cb5a57b47b3be19045788e52368
kaixiang <fatty_xiang@hotmail.com>
2017-06-17 11:06:47 +0800

    commit to repo

--- myFile/Form1.Designer.vb
+++ myFile/Form1.Designer.vb
@@ -30,9 +30,9 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(317, 162)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Hello world :)"
+        Me.Label1.Text = "Hello worlds :)"
         '
         'Form1
         '
