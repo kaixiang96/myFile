8e3bedf4480fd3c0192040a29c5a2814d44ed861
kaixiang <fatty_xiang@hotmail.com>
2017-06-17 11:14:24 +0800

    commit to branch

--- myFile/Form1.Designer.vb
+++ myFile/Form1.Designer.vb
@@ -30,9 +30,9 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(317, 162)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(123, 13)
+        Me.Label1.Size = New System.Drawing.Size(122, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Hello worlds my friends :)"
+        Me.Label1.Text = "I now at the new branch"
         '
         'Form1
         '
